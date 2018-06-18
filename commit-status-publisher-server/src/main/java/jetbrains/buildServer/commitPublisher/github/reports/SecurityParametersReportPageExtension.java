b2192eb52c919c45b10be6edfbc180ff3788a9e0
pavel.sher <pavel.sher@jetbrains.com>
2018-06-18 20:32:36 +0200

    fix TW-55617

--- commit-status-publisher-server/src/main/java/jetbrains/buildServer/commitPublisher/github/reports/SecurityParametersReportPageExtension.java
+++ commit-status-publisher-server/src/main/java/jetbrains/buildServer/commitPublisher/github/reports/SecurityParametersReportPageExtension.java
@@ -1,9 +1,13 @@
 package jetbrains.buildServer.commitPublisher.github.reports;
 
+import jetbrains.buildServer.serverSide.SBuildType;
+import jetbrains.buildServer.serverSide.SProject;
+import jetbrains.buildServer.serverSide.auth.Permission;
 import jetbrains.buildServer.serverSide.healthStatus.HealthStatusItem;
 import jetbrains.buildServer.web.openapi.PagePlaces;
 import jetbrains.buildServer.web.openapi.PluginDescriptor;
 import jetbrains.buildServer.web.openapi.healthStatus.HealthStatusItemPageExtension;
+import jetbrains.buildServer.web.util.SessionUser;
 import org.jetbrains.annotations.NotNull;
 
 import javax.servlet.http.HttpServletRequest;
@@ -24,5 +28,16 @@ public class SecurityParametersReportPageExtension extends HealthStatusItemPageE
   public void fillModel(@NotNull Map<String, Object> model, @NotNull HttpServletRequest request) {
     HealthStatusItem item = getStatusItem(request);
     model.putAll(item.getAdditionalData());
+  }
+
+  @Override
+  public boolean isAvailable(@NotNull final HttpServletRequest request) {
+    if (!super.isAvailable(request)) return false;
+
+    HealthStatusItem item = getStatusItem(request);
+    SBuildType bt = (SBuildType)item.getAdditionalData().get("buildType");
+    if (bt == null) return false;
+
+    return SessionUser.getUser(request).isPermissionGrantedForProject(bt.getProjectId(), Permission.VIEW_BUILD_CONFIGURATION_SETTINGS);
   }
 }
