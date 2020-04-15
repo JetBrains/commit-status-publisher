4c6feae4f0416ee5e26ba7014728742c2425d54f
Anton Zamolotskikh <anton.zamolotskikh@jetbrains.com>
2020-04-15 16:50:56 +0100

    TW-63716 Health report for username/password authentication that is deprecated in GitHub now
    (cherry picked from commit 03d16868671f7dedd87af771f268045fc9b974f3)

---
+++ commit-status-publisher-server/src/main/java/jetbrains/buildServer/commitPublisher/reports/DeprecatedAuthReport.java
@@ -0,0 +1,77 @@
+package jetbrains.buildServer.commitPublisher.reports;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import jetbrains.buildServer.commitPublisher.CommitStatusPublisherFeature;
+import jetbrains.buildServer.commitPublisher.CommitStatusPublisherSettings;
+import jetbrains.buildServer.commitPublisher.Constants;
+import jetbrains.buildServer.commitPublisher.PublisherManager;
+import jetbrains.buildServer.commitPublisher.github.api.GitHubApiAuthenticationType;
+import jetbrains.buildServer.serverSide.SBuildFeatureDescriptor;
+import jetbrains.buildServer.serverSide.SBuildType;
+import jetbrains.buildServer.serverSide.healthStatus.*;
+import org.jetbrains.annotations.NotNull;
+
+public class DeprecatedAuthReport extends HealthStatusReport {
+
+  private static final String REPORT_TYPE = "CommitStatusPublisherDeprecatedAuth";
+  private static final String DISPLAY_NAME
+          = "Commit Status Publisher build feature uses username/password authentication that is now deprecated in GitHub";
+  private static final ItemCategory CATEGORY
+          = new ItemCategory(REPORT_TYPE + "Category", DISPLAY_NAME, ItemSeverity.WARN);
+
+  private final PublisherManager myPublisherManager;
+
+  public DeprecatedAuthReport(@NotNull PublisherManager publisherManager) {
+    myPublisherManager = publisherManager;
+  }
+
+  @NotNull
+  @Override
+  public String getType() {
+    return REPORT_TYPE;
+  }
+
+  @NotNull
+  @Override
+  public String getDisplayName() {
+    return DISPLAY_NAME;
+  }
+
+  @NotNull
+  @Override
+  public Collection<ItemCategory> getCategories() {
+    return Collections.singleton(CATEGORY);
+  }
+
+  @Override
+  public boolean canReportItemsFor(@NotNull HealthStatusScope healthStatusScope) {
+    return healthStatusScope.isItemWithSeverityAccepted(ItemSeverity.WARN);
+  }
+
+  @Override
+  public void report(@NotNull HealthStatusScope scope, @NotNull HealthStatusItemConsumer consumer) {
+
+    for (SBuildType bt : scope.getBuildTypes()) {
+      Collection<SBuildFeatureDescriptor> features = bt.getBuildFeaturesOfType(CommitStatusPublisherFeature.TYPE);
+      for (SBuildFeatureDescriptor feature: features) {
+        if (bt.isEnabled(feature.getId())) {
+          Map<String, String> params = feature.getParameters();
+          String publisherId = params.get(Constants.PUBLISHER_ID_PARAM);
+          if (publisherId == null || !publisherId.equals(Constants.GITHUB_PUBLISHER_ID))
+            continue;
+          CommitStatusPublisherSettings settings = myPublisherManager.findSettings(publisherId);
+          if (null != settings && GitHubApiAuthenticationType.PASSWORD_AUTH.getValue().equals(params.get(Constants.GITHUB_AUTH_TYPE))) {
+            String identity = REPORT_TYPE + "_BT_" + bt.getInternalId() + "_FEATURE_" + feature.getId();
+            Map<String, Object> additionalData = new HashMap<>();
+            additionalData.put("buildType", bt);
+            additionalData.put("publisherType", settings.getName());
+            consumer.consumeForBuildType(bt, new HealthStatusItem(identity, CATEGORY, additionalData));
+          }
+        }
+      }
+    }
+  }
+}
---
+++ commit-status-publisher-server/src/main/java/jetbrains/buildServer/commitPublisher/reports/DeprecatedAuthReportPageExtension.java
@@ -0,0 +1,27 @@
+package jetbrains.buildServer.commitPublisher.reports;
+
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import jetbrains.buildServer.serverSide.healthStatus.HealthStatusItem;
+import jetbrains.buildServer.web.openapi.PagePlaces;
+import jetbrains.buildServer.web.openapi.PluginDescriptor;
+import jetbrains.buildServer.web.openapi.healthStatus.HealthStatusItemPageExtension;
+import org.jetbrains.annotations.NotNull;
+
+public class DeprecatedAuthReportPageExtension extends HealthStatusItemPageExtension {
+
+  public DeprecatedAuthReportPageExtension(@NotNull PagePlaces pagePlaces,
+                                           @NotNull DeprecatedAuthReport report,
+                                           @NotNull PluginDescriptor pluginDescriptor) {
+    super(report.getType(), pagePlaces);
+    setIncludeUrl(pluginDescriptor.getPluginResourcesPath("reports/deprecatedAuthReport.jsp"));
+    register();
+  }
+
+
+  @Override
+  public void fillModel(@NotNull Map<String, Object> model, @NotNull HttpServletRequest request) {
+    HealthStatusItem item = getStatusItem(request);
+    model.putAll(item.getAdditionalData());
+  }
+}
