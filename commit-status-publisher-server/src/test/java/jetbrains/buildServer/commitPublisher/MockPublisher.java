c24f59738a245fd6ceeffdc66fff88c1c51f0066
Anton Zamolotskikh <anton.zamolotskikh@jetbrains.com>
2020-06-05 17:07:46 +0100

    Test for the order of status publishing events (re TW-66395)

--- commit-status-publisher-server/src/test/java/jetbrains/buildServer/commitPublisher/MockPublisher.java
+++ commit-status-publisher-server/src/test/java/jetbrains/buildServer/commitPublisher/MockPublisher.java
@@ -1,14 +1,11 @@
 package jetbrains.buildServer.commitPublisher;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import jetbrains.buildServer.messages.Status;
 import jetbrains.buildServer.serverSide.*;
 import jetbrains.buildServer.users.User;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.Map;
 
 /**
  * @author anton.zamolotskikh, 13/09/16.
@@ -26,6 +23,7 @@ class MockPublisher extends BaseCommitStatusPublisher implements CommitStatusPub
   private boolean myShouldReportError = false;
   private int myFailuresReceived = 0;
   private int mySuccessReceived = 0;
+  private Set<Event> myEventsToWait = new HashSet<Event>();
 
   private final PublisherLogger myLogger;
 
@@ -64,6 +62,19 @@ class MockPublisher extends BaseCommitStatusPublisher implements CommitStatusPub
     myVcsRootId = vcsRootId;
   }
 
+  void setEventToWait(Event event) {
+    myEventsToWait.add(event);
+  }
+
+  void notifyWaitingEvent(Event event, long delayMs) throws InterruptedException {
+    if (myEventsToWait.contains(event)) {
+      Thread.sleep(delayMs);
+      synchronized (event) {
+        event.notify();
+      }
+    }
+  }
+
   @NotNull
   @Override
   public String getId() {
@@ -77,30 +88,44 @@ class MockPublisher extends BaseCommitStatusPublisher implements CommitStatusPub
   int successReceived() { return mySuccessReceived; }
 
   void shouldThrowException() {myShouldThrowException = true; }
+
   void shouldReportError() {myShouldReportError = true; }
+
+  private void pretendToHandleEvent(Event event) throws PublisherException {
+    if (myEventsToWait.contains(event)) {
+      try {
+        synchronized(event) {
+          event.wait(10000);
+        }
+      } catch (InterruptedException e) {
+        throw new PublisherException("Mock publisher interrupted", e);
+      }
+    }
+    myEventsReceived.add(event);
+  }
 
   @Override
   public boolean buildQueued(@NotNull final SQueuedBuild build, @NotNull final BuildRevision revision) throws PublisherException {
-    myEventsReceived.add(Event.QUEUED);
+    pretendToHandleEvent(Event.QUEUED);
     return true;
   }
 
   @Override
   public boolean buildRemovedFromQueue(@NotNull final SQueuedBuild build, @NotNull final BuildRevision revision, @Nullable final User user, @Nullable final String comment)
     throws PublisherException {
-    myEventsReceived.add(Event.REMOVED_FROM_QUEUE);
+    pretendToHandleEvent(Event.REMOVED_FROM_QUEUE);
     return true;
   }
 
   @Override
   public boolean buildStarted(@NotNull final SBuild build, @NotNull final BuildRevision revision) throws PublisherException {
-    myEventsReceived.add(Event.STARTED);
+    pretendToHandleEvent(Event.STARTED);
     return true;
   }
 
   @Override
   public boolean buildFinished(@NotNull SBuild build, @NotNull BuildRevision revision) throws PublisherException {
-    myEventsReceived.add(Event.FINISHED);
+    pretendToHandleEvent(Event.FINISHED);
     Status s = build.getBuildStatus();
     if (s.equals(Status.NORMAL)) mySuccessReceived++;
     if (s.equals(Status.FAILURE)) myFailuresReceived++;
@@ -119,27 +144,27 @@ class MockPublisher extends BaseCommitStatusPublisher implements CommitStatusPub
                                 @Nullable final String comment,
                                 final boolean buildInProgress)
     throws PublisherException {
-    myEventsReceived.add(Event.COMMENTED);
+    pretendToHandleEvent(Event.COMMENTED);
     myLastComment = comment;
     return true;
   }
 
   @Override
   public boolean buildInterrupted(@NotNull final SBuild build, @NotNull final BuildRevision revision) throws PublisherException {
-    myEventsReceived.add(Event.INTERRUPTED);
+    pretendToHandleEvent(Event.INTERRUPTED);
     return true;
   }
 
   @Override
-  public boolean buildFailureDetected(@NotNull SBuild build, @NotNull BuildRevision revision) {
-    myEventsReceived.add(Event.FAILURE_DETECTED);
+  public boolean buildFailureDetected(@NotNull SBuild build, @NotNull BuildRevision revision) throws PublisherException {
+    pretendToHandleEvent(Event.FAILURE_DETECTED);
     myFailuresReceived++;
     return true;
   }
 
   @Override
   public boolean buildMarkedAsSuccessful(@NotNull SBuild build, @NotNull BuildRevision revision, boolean buildInProgress) throws PublisherException {
-    myEventsReceived.add(Event.MARKED_AS_SUCCESSFUL);
+    pretendToHandleEvent(Event.MARKED_AS_SUCCESSFUL);
     return super.buildMarkedAsSuccessful(build, revision, buildInProgress);
   }
 }
