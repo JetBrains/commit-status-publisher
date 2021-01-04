9240442f9e3eed52be89c0afa53030419bf71ef4
Anton Zamolotskikh <anton.zamolotskikh@jetbrains.com>
2021-01-04 16:24:10 +0000

    Minor cleanup: remove unused imports in StashPublisherTest

--- commit-status-publisher-server/src/test/java/jetbrains/buildServer/commitPublisher/stash/StashPublisherTest.java
+++ commit-status-publisher-server/src/test/java/jetbrains/buildServer/commitPublisher/stash/StashPublisherTest.java
@@ -1,22 +1,7 @@
 package jetbrains.buildServer.commitPublisher.stash;
 
-import com.google.gson.Gson;
-import java.util.Collections;
-import jetbrains.buildServer.commitPublisher.Constants;
-import jetbrains.buildServer.commitPublisher.HttpPublisherTest;
-import jetbrains.buildServer.commitPublisher.MockPluginDescriptor;
-import jetbrains.buildServer.commitPublisher.stash.data.StashRepoInfo;
-import jetbrains.buildServer.commitPublisher.stash.data.StashServerInfo;
-import jetbrains.buildServer.serverSide.BuildRevision;
-import jetbrains.buildServer.vcs.VcsRootInstance;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpResponse;
-import org.apache.http.entity.StringEntity;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Test
 public class StashPublisherTest extends BaseStashPublisherTest {
