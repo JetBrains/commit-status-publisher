3edd3a5b534674c12fc41933942cd32a4303f20a
vasiliy.khutorov <vasiliy.khutorov@jetbrains.com>
2022-01-28 15:00:27 +0000

    TW-73850 Add compatibility with BitBucket's checks for merging pull requests
    Merge-request: TC-MR-2417

--- commit-status-publisher-server/src/main/java/jetbrains/buildServer/commitPublisher/stash/data/JsonStashBuildStatus.java
+++ commit-status-publisher-server/src/main/java/jetbrains/buildServer/commitPublisher/stash/data/JsonStashBuildStatus.java
@@ -17,7 +17,7 @@
 package jetbrains.buildServer.commitPublisher.stash.data;
 
 public class JsonStashBuildStatus {
-  public String buildNumber, description, key, name, ref, state, url;
+  public String buildNumber, description, key, parent, name, ref, state, url;
   public long duration;
   public StashTestStatistics testResults;
   public static class StashTestStatistics {
