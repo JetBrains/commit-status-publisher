7e9a9123851b121efd08b94357bd289975c54413
vasiliy.khutorov <vasiliy.khutorov@jetbrains.com>
2022-09-09 10:32:45 +0200

    TW-77184 Optimized processing of new constants

--- commit-status-publisher-server/src/main/java/jetbrains/buildServer/CommitStatusPublisherConstants.java
+++
@@ -1,6 +0,0 @@
-package jetbrains.buildServer;
-
-public interface CommitStatusPublisherConstants {
-  String STATUSES_TO_LOAD_THRESHOLD_PROPERTY = "teamcity.commitStatusPubliser.statusesToLoad.threshold";
-  int STATUSES_TO_LOAD_THRESHOLD_DEFAULT_VAL = 50;
-}
--- commit-status-publisher-server/src/main/java/jetbrains/buildServer/commitPublisher/Constants.java
+++ commit-status-publisher-server/src/main/java/jetbrains/buildServer/commitPublisher/Constants.java
@@ -70,6 +70,8 @@ public class Constants {
   public static final String GITLAB_PUBLISHER_ID = "gitlabStatusPublisher";
   public static final String GITLAB_API_URL = "gitlabApiUrl";
   public static final String GITLAB_TOKEN = "secure:gitlabAccessToken";
+  public static final String STATUSES_TO_LOAD_THRESHOLD_PROPERTY = "teamcity.commitStatusPubliser.statusesToLoad.threshold";
+  public static final int STATUSES_TO_LOAD_THRESHOLD_DEFAULT_VAL = 50;
 
 
   @NotNull
