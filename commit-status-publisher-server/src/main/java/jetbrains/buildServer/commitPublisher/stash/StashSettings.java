f81809bc6e3c059025672a72493da3cc62446011
Andreas Eisele <andreas.eisele@jetbrains.com>
2023-09-06 15:56:21 +0000

    TW-81718: unify display name of stash publisher -> "Bitbucket Server / Data Center"
    Merge-request: TC-MR-6793
    Merged-by: Andreas Eisele <Andreas.Eisele@jetbrains.com>

--- commit-status-publisher-server/src/main/java/jetbrains/buildServer/commitPublisher/stash/StashSettings.java
+++ commit-status-publisher-server/src/main/java/jetbrains/buildServer/commitPublisher/stash/StashSettings.java
@@ -91,7 +91,7 @@ public class StashSettings extends AuthTypeAwareSettings implements CommitStatus
 
   @NotNull
   public String getName() {
-    return "Bitbucket Server (Atlassian Stash)";
+    return "Bitbucket Server / Data Center";
   }
 
   @Nullable
