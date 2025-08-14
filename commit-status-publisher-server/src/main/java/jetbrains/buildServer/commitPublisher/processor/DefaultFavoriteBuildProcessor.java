12a2105bdaa7096542e791f7fd096d57bfb1ca06
matteo.iorio <matteo.iorio01@gmail.com>
2025-08-14 17:17:58 +0200

    feat: add FavoriteBuildProcessor implementation, use FavoriteBuildsManager for tagging a build as favorite. Implement `shouldMarkBuild` by checking the user's checkbox value. Implement `markAsFavorite` by filtering users using predicate `shouldMarkAsFavorite`

--- commit-status-publisher-server/src/main/java/jetbrains/buildServer/commitPublisher/processor/DefaultFavoriteBuildProcessor.java
+++ commit-status-publisher-server/src/main/java/jetbrains/buildServer/commitPublisher/processor/DefaultFavoriteBuildProcessor.java
@@ -1,4 +1,35 @@
 package jetbrains.buildServer.commitPublisher.processor;
 
-public class DefaultFavoriteBuildProcessor {
+import jetbrains.buildServer.commitPublisher.Constants;
+import jetbrains.buildServer.commitPublisher.processor.suppplier.BuildOwnerStrategy;
+import jetbrains.buildServer.favoriteBuilds.FavoriteBuildsManager;
+import jetbrains.buildServer.serverSide.BuildPromotion;
+import jetbrains.buildServer.serverSide.SBuild;
+import jetbrains.buildServer.users.PropertyKey;
+import jetbrains.buildServer.users.SUser;
+import jetbrains.buildServer.users.SimplePropertyKey;
+import org.jetbrains.annotations.NotNull;
+
+public class DefaultFavoriteBuildProcessor implements FavoriteBuildProcessor{
+
+  private static final PropertyKey USER_PROPERTY = new SimplePropertyKey(Constants.USER_AUTOMATICALLY_MARK_IMPORTANT_BUILDS_AS_FAVORITE_INTERNAL_PROPERTY);
+  private final FavoriteBuildsManager myFavoriteBuildsManager;
+
+  public DefaultFavoriteBuildProcessor(@NotNull FavoriteBuildsManager favoriteBuildsManager) {
+    myFavoriteBuildsManager = favoriteBuildsManager;
+  }
+
+  @Override
+  public boolean shouldMarkAsFavorite(@NotNull SUser user) {
+    return user.getBooleanProperty(USER_PROPERTY);
+  }
+
+  @Override
+  public void markAsFavorite(@NotNull SBuild build, @NotNull BuildOwnerStrategy buildOwnerStrategy) {
+    final BuildPromotion buildPromotion = build.getBuildPromotion();
+    buildOwnerStrategy.apply(build)
+      .stream()
+      .filter(this::shouldMarkAsFavorite)
+      .forEach(candidate -> myFavoriteBuildsManager.tagBuild(buildPromotion, candidate));
+  }
 }
